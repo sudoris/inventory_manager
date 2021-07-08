dc10832e7cf900eaec05756ed0d389e1b5480cb7
David Chen <zdent09@gmail.com>
2021-07-08 20:57:08 +0800

    initialize and set up nav drawer and layout

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,91 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="leftDrawerOpen = !leftDrawerOpen" />
+
+        <q-toolbar-title>          
+          Our Project
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" content-class="bg-grey-1" bordered>
+      <q-list>
+        <div class="drawer-item">
+          <q-btn outline color="accent" label="Welcome" />
+        </div>
+        <SectionLink
+          v-for="link in sectionLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>  
+</template>
+
+<script>
+import SectionLink from 'src/components/SectionLink.vue'
+
+const linksData = [
+  {
+    title: 'Inventory',
+    icon: 'o_inventory',
+    link: '/'
+  },
+  {
+    title: 'Orders',
+    icon: 'o_local_shipping',
+    link: '/'
+  },
+  {
+    title: 'Reports',
+    icon: 'o_assessment',
+    link: '/'
+  }  
+];
+
+export default {
+  name: 'MainLayout',
+  components: { SectionLink },
+  data () {
+    return {
+      leftDrawerOpen: false,
+      sectionLinks: linksData
+    }
+  }
+}
+</script>
+
+<style scoped>
+.drawer-column {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+.drawer-bottom { 
+  align-self: flex-end;
+  min-width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.block-icon {
+  display: inline-block;
+}
+
+.drawer-item {
+  min-width: 100%;
+  padding: 16px 8px;
+  display: flex;
+  justify-content: center;
+}
+</style>
